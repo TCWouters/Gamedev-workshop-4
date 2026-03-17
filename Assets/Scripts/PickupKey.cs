aaa63fca185784d4e197f98230a11e2f22bc7e0f
TCWouters <TC.Wouters1@student.han.nl>
2026-03-17 14:23:59 +0100

    deur werkt (goed genoeg)

--- Assets/Scripts/PickupKey.cs
+++ Assets/Scripts/PickupKey.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class PickupKey : MonoBehaviour
 {
-    public Collider vaultCollider;
+    public static bool hasKey = false;
 
     void OnTriggerEnter(Collider other)
     {
         // Check if the object that entered the trigger is the player
         if (other.CompareTag("Player"))
-        {
-            vaultCollider.enabled = false; // animation needed
-            
+        {  
+            hasKey = true;
+            // vaultCollider.enabled = false; // animation needed
+            Debug.Log("Key picked up!");
             Destroy(gameObject);
         }
     }
