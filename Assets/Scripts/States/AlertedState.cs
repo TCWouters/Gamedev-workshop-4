27647dc968a27996e0f68f426221f2f404b32f4b
noahvolman <N.Volman@student.han.nl>
2026-03-17 13:02:46 +0100

    eerste blueprint states

---
+++ Assets/Scripts/States/AlertedState.cs
@@ -0,0 +1,2 @@
+// de enemy hoort wat en gaat in de richting van het geluid of naar de speler die hij gezien of gehoord heeft. Hij gaat terug naar zijn IdleState
+// als hij de speler niet meer ziet of hoort.
---
+++ Assets/Scripts/States/IdleState.cs
@@ -0,0 +1,3 @@
+//De enemy patrouilleert een gebied. Hij heeft een aantal waypoints die hij afloopt. Als hij de speler ziet of hoort gaat hij naar zijn AlertedState.
+// Als hij de speler niet meer ziet of hoort gaat hij terug naar zijn IdleState.
+
